ec0f5b1b5195a670b26960e946218a556fcf7e35
Azza Maddouri <90465414+azzamaddouri@users.noreply.github.com>
2026-05-03 07:09:10 -0700

    (Fix): Rename class name to ShipmentTrackerApplication

--- backend/src/main/java/com/apalindromestring/shipmenttracker/ShipmentTrackerApplication.java
+++ backend/src/main/java/com/apalindromestring/shipmenttracker/ShipmentTrackerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShipmenttrackerApplication {
+public class ShipmentTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShipmenttrackerApplication.class, args);
