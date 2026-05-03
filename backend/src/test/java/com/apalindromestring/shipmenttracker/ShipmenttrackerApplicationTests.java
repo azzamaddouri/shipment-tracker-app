7147d95d592cfd8535a701be7c880329e51807d4
azzamaddouri <maddouriazza@gmail.com>
2026-05-03 06:52:49 -0700

    (setup): setup the backend project

---
+++ backend/src/test/java/com/apalindromestring/shipmenttracker/ShipmenttrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apalindromestring.shipmenttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmenttrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
