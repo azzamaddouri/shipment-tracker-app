d42460f4773e78ae57d8737e6f52c5cfd6b14c65
azzamaddouri <maddouriazza@gmail.com>
2026-05-12 09:02:22 -0700

    impl(frontend): add notification component for real-time shipment updates

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,10 +3,11 @@ import { ShipmentGrid } from './components/shipment-grid/shipment-grid';
 import { CreateShipment } from './components/create-shipment/create-shipment';
 import { UpdateShipment } from './components/update-shipment/update-shipment';
 import { Header } from './components/header/header';
+import { Notification } from './components/notification/notification';
 
 @Component({
   selector: 'app-root',
-  imports: [ShipmentGrid,CreateShipment,UpdateShipment,Header],
+  imports: [ShipmentGrid,CreateShipment,UpdateShipment,Header,Notification],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ frontend/src/app/components/notification/notification.ts
@@ -0,0 +1,48 @@
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { WebsocketService } from '../../services/websocket/websocket-service';
+import { SHIPMENT_STATUS, ShipmentStatus, STATUS_LABELS, StatusUpdateMessage } from '../../models/shipment.model';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-notification',
+  imports: [CommonModule],
+  templateUrl: './notification.html',
+  styleUrl: './notification.css',
+})
+export class Notification implements OnInit {
+
+  private destroyRef = inject(DestroyRef);
+  private websocketService = inject(WebsocketService);
+
+  STATUS_LABELS = STATUS_LABELS
+  notifications = signal<StatusUpdateMessage[]>([]);
+
+ readonly STATUS_BADGE_CLASS_MAP: Record<ShipmentStatus, string> = {
+    [SHIPMENT_STATUS.ORDER_PLACED]: 'bg-blue-500/10 text-blue-400 ring-blue-500/20',
+    [SHIPMENT_STATUS.PROCESSING]: 'bg-amber-500/10 text-amber-400 ring-amber-500/20',
+    [SHIPMENT_STATUS.PICKED_UP]: 'bg-purple-500/10 text-purple-400 ring-purple-500/20',
+    [SHIPMENT_STATUS.IN_TRANSIT]: 'bg-cyan-500/10 text-cyan-400 ring-cyan-500/20',
+    [SHIPMENT_STATUS.OUT_FOR_DELIVERY]: 'bg-yellow-500/10 text-yellow-400 ring-yellow-500/20',
+    [SHIPMENT_STATUS.DELIVERED]: 'bg-green-500/10 text-green-400 ring-green-500/20',
+    [SHIPMENT_STATUS.EXCEPTION]: 'bg-red-500/10 text-red-400 ring-red-500/20',
+  };
+
+  getStatusBadgeClass(status: ShipmentStatus): string {
+    return this.STATUS_BADGE_CLASS_MAP[status];
+  }
+
+  ngOnInit(): void {
+    this.handleUpdate();
+  }
+
+  private handleUpdate(): void {
+    this.websocketService.getStatusUpdates()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((update) => {
+        if (update) {
+          this.notifications.update((notifications) => [update, ...notifications]);
+        }
+      });
+  }
+}
