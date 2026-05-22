39b5826a0feff6fc87ea91d358d113794e00e464
azzamaddouri <maddouriazza@gmail.com>
2026-05-22 09:52:27 -0700

    refactor(routes): update component imports and paths for consistency

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,54 +2,56 @@ import { Routes } from '@angular/router';
 import { publicGuard } from './core/guards/public/public-guard';
 import { authGuard } from './core/guards/auth/auth-guard';
 import { roleGuard } from './core/guards/role/role-guard';
-import { OperatorShell } from './layout/operator-shell/operator-shell';
-import { OperatorDashboard } from './features/operator/operator-dashboard/operator-dashboard';
-import { CarrierDashboard } from './features/carrier/carrier-dashboard/carrier-dashboard';
+import { OperatorShellComponent } from './layout/operator-shell/operator-shell.component';
+import { OperatorDashboardComponent } from './features/operator/operator-dashboard/operator-dashboard.component';
+import { CarrierShellComponent } from './layout/carrier-shell/carrier-shell.component';
+import { CarrierDashboardComponent } from './features/carrier/carrier-dashboard/carrier-dashboard.component';
+;
 
 export const routes: Routes = [
 
     {
         path:'',
         loadComponent: ()=>
-            import('./features/public/home/home')
-        .then(m => m.Home)
+            import('./features/public/home/home.component')
+        .then(m => m.HomeComponent)
     },
 
     {
         path:'track/:trackingNumber',
         loadComponent : () =>
-            import('./features/public/shipment-tracking/shipment-tracking')
-        .then(m => m.ShipmentTracking)
+            import('./features/public/shipment-tracking/shipment-tracking.component')
+        .then(m => m.ShipmentTrackingComponent)
     },
 
     {
-        path:'/login',
+        path:'login',
         canActivate : [publicGuard],
         loadComponent: () =>
-            import('./features/public/login/login')
-        .then(m => m.Login)
+            import('./features/public/login/login.component')
+        .then(m => m.LoginComponent)
 
     },
 
     {
-        path:'/operator',
-        component: OperatorShell,
+        path:'operator',
+        component: OperatorShellComponent,
         canActivate:[authGuard, roleGuard('OPERATOR')],
         children:[
             {path: '', redirectTo:'dashboard', pathMatch:'full'},
-            { path:'dashboard', component:OperatorDashboard}
+            { path:'dashboard', component:OperatorDashboardComponent}
 
         ]
         
     },
 
     {
-        path:'/carrier',
-        component: OperatorShell,
+        path:'carrier',
+        component: CarrierShellComponent,
         canActivate:[authGuard, roleGuard('CARRIER')],
         children:[
             {path: '', redirectTo:'dashboard', pathMatch:'full'},
-            { path:'dashboard', component:CarrierDashboard}
+            { path:'dashboard', component:CarrierDashboardComponent}
 
         ]
         
